fd3a3b74f11fec2842a9c5b62843fd90fba194b8
Edgar Vergara <edgarvm13@hotmail.com>
2024-10-10 17:40:12 +0200

    ✨ pokemons resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { PokemonsModule } from './pokemons/pokemons.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    PokemonsModule,
   ],
 })
 export class AppModule {}

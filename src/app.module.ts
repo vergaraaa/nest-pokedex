5d2bc9834a13b36003ad21261ce33013841db230
Edgar Vergara <edgarvm13@hotmail.com>
2024-10-15 05:53:06 +0200

    🚧 seed

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 
 import { PokemonsModule } from './pokemons/pokemons.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { CommonModule } from './common/common.module';
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     CommonModule,
     PokemonsModule,
+    SeedModule,
   ],
 })
 export class AppModule {}
